3100fc0683cd184c0db3f0866ce37fb82a89c8cf
Tin Lam <stannum-l@users.noreply.github.com>
2020-03-02 23:35:29 -0600

    Add golangci-lint
    This patch set places in a golangci-lint to the repository's github
    action workflow

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "windwaker",
-	Short: "Wind Waker",
-}
+const (
+	ERR = 1
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "windwaker",
+		Short: "Wind Waker",
+	}
+)
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(ERR)
 	}
 }
